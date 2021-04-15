dae20717c22a824cccd060df33f40fa7e2910153
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-04-15 10:04:16 +0200

    Started to work on project for my client

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="login.css">
+        <title>Login Page</title>
+    </head>
+    <body>
+        <form method="post" action="" name="signin-form">
+            <div class="form-element">
+                <label>Username</label>
+                <input type="text" name="username" pattern="[a-zA-Z0-9]+" required />
+            </div>
+            <div class="form-element">
+                <label>Password</label>
+                <input type="password" name="password" required />
+            </div>
+            <button type="submit" name="login" value="login">Log In</button>
+        </form>
+    </body>
+</html>
